a2af6387a8db7e42c84bf835746ef3946301eede
Ramanspec <115243223+Ramanspec@users.noreply.github.com>
2022-10-18 15:30:23 +0100

    Dosing should be in protocol, not model

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -12,8 +12,7 @@ class Model:
         an example paramter
 
     """
-   Dose(t)=Dose_initial+[Dose_pulsed if t/T_{interval}== int] + Dose_continuous*t#with all these Dose_ being set by the user and the if statement to make dosed injection possible
-    
+   
     for i <= number_peripherals #with number_peripherals an integer float chosen by the user
     
         if chosen_model== intravenous_bolus
