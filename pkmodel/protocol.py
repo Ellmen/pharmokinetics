26ef8557c5992f353da915ea4bebe5ed4c82ca0a
Ramanspec <115243223+Ramanspec@users.noreply.github.com>
2022-10-18 15:30:46 +0100

    Update protocol.py

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -12,6 +12,9 @@ class Protocol:
         an example paramter
 
     """
+    
+       Dose(t)=Dose_initial+[Dose_pulsed if t/T_{interval}== int] + Dose_continuous*t#with all these Dose_ being set by the user and the if statement to make dosed injection possible
+
     def __init__(self, value=43):
         self.value = value
 
