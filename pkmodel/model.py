f8b0e0464b0cbbd47c61cffbb982e13ec132aeb3
Leonard Lee <leonard.lee1@outlook.com>
2022-10-19 12:17:13 +0100

    https://github.com/Ellmen/pharmokinetics/issues/3

--- pkmodel/model.py
+++ pkmodel/model.py
@@ -3,32 +3,51 @@
 #
 
 class Model:
-    """A Pharmokinetic (PK) model
-
-    Parameters
-    ----------
-
-    value: numeric, optional
-        an example paramter
-
-    """
-   
-    for i <= number_peripherals #with number_peripherals an integer float chosen by the user
+    # for i <= number_peripherals #with number_peripherals an integer float chosen by the user
     
-        if chosen_model== intravenous_bolus
+    #     if chosen_model== intravenous_bolus
 
-        dq_c/dt=Dose(t) -q_c/V_c*CL-Q_{p1}*(q_c/V_c - q_{p1}/V_{p1})
-        d_{qp1}/dt=Q_{p1}*(q_c/V_c - q_{p1}/V_{p1})
-        #TODO: add code to include possibility for more than 1 peripheral compartment
+    #     dq_c/dt=Dose(t) -q_c/V_c*CL-Q_{p1}*(q_c/V_c - q_{p1}/V_{p1})
+    #     d_{qp1}/dt=Q_{p1}*(q_c/V_c - q_{p1}/V_{p1})
+    #     #TODO: add code to include possibility for more than 1 peripheral compartment
 
-        elif chosen_model==subcutaneous
+    #     elif chosen_model==subcutaneous
 
-        d_{q0}/dt=Dose(t)-k_a*q_0
-        dq_c/dt=k_aq*q_0-q_c/V_c*CL-Q_{p1}*(q_c/V_c - q_{p1}/V_{p1})
-        #TODO: add code to include possibility for more than 1 peripheral compartment
-        d_{qp1}/dt=Q_{p1}*(q_c/V_c - q_{p1}/V_{p1})
+    #     d_{q0}/dt=Dose(t)-k_a*q_0
+    #     dq_c/dt=k_aq*q_0-q_c/V_c*CL-Q_{p1}*(q_c/V_c - q_{p1}/V_{p1})
+    #     #TODO: add code to include possibility for more than 1 peripheral compartment
+    #     d_{qp1}/dt=Q_{p1}*(q_c/V_c - q_{p1}/V_{p1})
         
     
-    def __init__(self, value=42):
-        self.value = value
+    def __init__(self, dosing=None, peripherals=None, therapeutic_max=None, therapeutic_min=None):
+        """A Pharmacokinetic (PK) model
 
+        Args:
+            dosing (Compartment): _description_
+            peripherals (Compartment): _description_
+            therapeutic_max (float): _description_
+            therapeutic_min (float): _description_
+        """
+        self.dosing = dosing
+        self.peripherals = peripherals
+        self.therapeutic_max = therapeutic_max
+        self.therapeutic_min = therapeutic_min
+        
+class Compartment:
+
+    def __init__(self, type, rate, volume, quantity) -> None:
+        self.type = type
+        self.rate = rate
+        self.volume = volume
+        self.quantity = quantity
+    
+    @property
+    def volume(self):
+        return self._volume
+    
+    @volume.setter
+    def volume(self, volume):
+        if self.type == "dosing":
+            self._volume = 0
+        else:
+            self._volume = volume
