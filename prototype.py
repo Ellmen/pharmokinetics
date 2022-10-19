2d72f8eb39f637be4c53736f34270eebe638b374
Ramanspec <115243223+Ramanspec@users.noreply.github.com>
2022-10-19 14:59:11 +0100

    added therapeutic window & dosing functionality

--- prototype.py
+++ prototype.py
@@ -2,13 +2,21 @@ import matplotlib.pylab as plt
 import numpy as np
 import scipy.integrate
 
-def dose(t, X):
-    return X
+therapeutic_min=1 #TODO: Remove when class defining these is up and running
+therapeutic_max=3
 
-def rhs(t, y, Q_p1, V_c, V_p1, CL, X):
+def dose(t, Xs):
+    dosage = 0
+    for X in Xs:
+        if t > X[0] and t < X[1]:
+            dosage += X[2]
+    return dosage
+
+
+def rhs(t, y, Q_p1, V_c, V_p1, CL, Xs):
     q_c, q_p1 = y
     transition = Q_p1 * (q_c / V_c - q_p1 / V_p1)
-    dqc_dt = dose(t, X) - q_c / V_c * CL - transition
+    dqc_dt = dose(t, Xs) - q_c / V_c * CL - transition
     dqp1_dt = transition
     return [dqc_dt, dqp1_dt]
 
@@ -18,7 +26,7 @@ model1_args = {
     'V_c': 1.0,
     'V_p1': 1.0,
     'CL': 1.0,
-    'X': 1.0,
+    'Xs': [[0,1,6]],
 }
 
 model2_args = {
@@ -27,7 +35,7 @@ model2_args = {
     'V_c': 1.0,
     'V_p1': 1.0,
     'CL': 1.0,
-    'X': 1.0,
+    'Xs': [[0,0.001,4000],[0.5,0.501,2000]], #figure out why the second spike does not get plotted
 }
 
 t_eval = np.linspace(0, 1, 1000)
@@ -36,7 +44,7 @@ y0 = np.array([0.0, 0.0])
 fig = plt.figure()
 for model in [model1_args, model2_args]:
     args = [
-        model['Q_p1'], model['V_c'], model['V_p1'], model['CL'], model['X']
+        model['Q_p1'], model['V_c'], model['V_p1'], model['CL'], model['Xs']
     ]
     sol = scipy.integrate.solve_ivp(
         fun=lambda t, y: rhs(t, y, *args),
@@ -45,8 +53,11 @@ for model in [model1_args, model2_args]:
     )
     plt.plot(sol.t, sol.y[0, :], label=model['name'] + '- q_c')
     plt.plot(sol.t, sol.y[1, :], label=model['name'] + '- q_p1')
+    if np.max(sol.y) > therapeutic_max:
+        print('Drug concentration of ' + model['name'] + ' exceeds toxic threshold, use lighter dosing')
 
+[plt.axhline(y=i, linestyle='--') for i in [therapeutic_min,therapeutic_max]]
 plt.legend()
 plt.ylabel('drug mass [ng]')
 plt.xlabel('time [h]')
-plt.show()
+plt.savefig('output.png') #Change .plot into .savefig for the VM to run it
