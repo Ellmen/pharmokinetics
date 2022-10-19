542c9e4e915ccef152d398894758ab5d39831430
Leonard Lee <leonard.lee1@outlook.com>
2022-10-19 13:41:02 +0100

    Added protocol interface

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -13,8 +13,33 @@ class Protocol:
 
     """
     
-       Dose(t)=Dose_initial+[Dose_pulsed if t/T_{interval}== int] + Dose_continuous*t#with all these Dose_ being set by the user and the if statement to make dosed injection possible
+    #    Dose(t)=Dose_initial+[Dose_pulsed if t/T_{interval}== int] + Dose_continuous*t
+    #    with all these Dose_ being set by the user and the if statement to make dosed injection possible
 
-    def __init__(self, value=43):
-        self.value = value
+    def __init__(self, dosing_mode, rate, dosing_strategy):
+        """A PK model
+
+        Args:
+            dosing_mode (_type_): "constant" or "spiked"
+            rate (_type_): rate of drug administration, ignore if "spiked"
+            dosing_strategy (_type_): list of tuples specifying intervals 
+                between time and the dose administered -- TO SPECIFY LATER. 
+                Ignore if dosing mode is "constant"
+        """
+        self.dosing_mode = dosing_mode
+        self.rate = rate
+        self.dosing_strategy = dosing_strategy
+    
+    @property
+    def dosing_mode(self):
+        return self._dosing_mode
+    
+    @dosing_mode.setter
+    def dosing_mode(self, dosing_mode):
+    # Validate that the dosing mode provided is valid before setting; if not 
+    # raise error
+        self._dosing_mode = dosing_mode
+
+    # Implement similar validation using setters and getters for rate and
+    # dosing strategy
 
