231104b2133303af60c314fb60d5e0428223f5af
Leonard Lee <leonard.lee1@outlook.com>
2022-10-20 15:03:59 +0100

    Modified compartment tests

--- pkmodel/tests/test_compartment.py
+++ pkmodel/tests/test_compartment.py
@@ -4,19 +4,10 @@ import pkmodel as pk
 class CompartmentTest(unittest.TestCase):
     """Tests the compartment class"""
     
-    def test_create_peripheral(self):
-        c = pk.Compartment("peripheral", 5, 5)
-        self.assertEqual(c.c_type, "peripheral")
+    def test_create_compartment(self):
+        c = pk.Compartment(5, 5)
         self.assertEqual(c.rate, 5)
         self.assertEqual(c.volume, 5)
-
-    def test_create_dosing(self):
-        c = pk.Compartment("dosing", 5, 5)
-        self.assertEqual(c.volume, 0)
-    
-    def test_invalid_compartment(self):
-        with self.assertRaises(ValueError):
-            c = pk.Compartment("sdf", 1, 4)
 
 if __name__ == "__main__":
     unittest.main()
