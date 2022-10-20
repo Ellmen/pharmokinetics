e5b1e0836342307d6d043f138d37fa2251c091a3
Ramanspec <115243223+Ramanspec@users.noreply.github.com>
2022-10-20 15:30:24 +0100

    Added docstrings

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -6,11 +6,12 @@ from pkmodel.dose import Dose, SpikeDose
 
 class Protocol:
     def __init__(self, dosing_strategy):
-        """A Pharmacokinetic protocol of doses
+        """ Define the drug dose administration protocol
 
         Args:
             dosing_strategy (list): list of doses specifying intervals
                 between time and the dose administered.
+             
         """
         self.dosing_strategy = dosing_strategy
         if not isinstance(dosing_strategy, list):
