000fabbf2603782d6bf9664cc5028d12da8d570e
Ramanspec <115243223+Ramanspec@users.noreply.github.com>
2022-10-20 16:35:14 +0100

    Docstrings added
    Not sure what to do always, see "TODO"

--- pkmodel/compartment.py
+++ pkmodel/compartment.py
@@ -3,18 +3,31 @@
 #
 
 class Compartment:
-
     def __init__(self, c_type, rate, volume) -> None:
+        """TODO
+        
+        Args:
+            c_type (TODO): Compartment type: Dosing or peripheral compartment.
+            rate (float): Flow rate of drug between two compartments.
+            volume (float): Volume (mL) inside a compartment.
+    """
         self.c_type = c_type  # Dosing or peripheral
         self.rate = rate
         self.volume = volume
 
     @property
     def volume(self):
+        """TODO
+    """
         return self._volume
 
     @volume.setter
     def volume(self, volume):
+        """TODO
+        
+        Args:
+            volume (float): Volume (mL) inside a compartment.
+        """
         if self.c_type == "dosing":
             self._volume = 0
         elif self.c_type == "peripheral":
