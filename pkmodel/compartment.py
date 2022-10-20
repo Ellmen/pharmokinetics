4f61bf2859c52e2dbe274b8c70a6885c1f71266b
Leonard Lee <leonard.lee1@outlook.com>
2022-10-20 14:58:50 +0100

    Implementation of dosing compartment

--- pkmodel/compartment.py
+++ pkmodel/compartment.py
@@ -4,20 +4,6 @@
 
 class Compartment:
 
-    def __init__(self, c_type, rate, volume) -> None:
-        self.c_type = c_type  # Dosing or peripheral
+    def __init__(self, rate, volume) -> None:
         self.rate = rate
         self.volume = volume
-
-    @property
-    def volume(self):
-        return self._volume
-
-    @volume.setter
-    def volume(self, volume):
-        if self.c_type == "dosing":
-            self._volume = 0
-        elif self.c_type == "peripheral":
-            self._volume = volume
-        else:
-            raise ValueError("'c_type' must be 'dosing' or 'peripheral'")
