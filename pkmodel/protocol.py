185188cc3ff9b12644ab3d8f520047147cb37f73
Isaac Ellmen <isaacellmen@gmail.com>
2022-10-20 11:53:53 +0100

    Protocol class cleanup

--- pkmodel/protocol.py
+++ pkmodel/protocol.py
@@ -13,22 +13,12 @@ class Protocol:
                 between time and the dose administered.
         """
         self.dosing_strategy = dosing_strategy
-
-    @property
-    def dosing_strategy(self):
-        return self._dosing_strategy
-
-    @dosing_strategy.setter
-    def dosing_strategy(self, dosing_strategy):
-        if len(dosing_strategy) == 0:
-            raise KeyError("A dosing strategy must be specified")
         if not isinstance(dosing_strategy, list):
             raise TypeError("Dosing strategy must be list of Dose objects.")
-        if len(dosing_strategy) == 1:
-            if not isinstance(dose, Dose):
-                raise KeyError("List must contain Dose or Spiked objects.")
-        else: # Greater than 1; must be a spiked dose protocol
+        elif len(dosing_strategy) == 0:
+            raise ValueError("A dosing strategy must be specified")
+        else:
             for dose in dosing_strategy:
-                if not isinstance(dose, SpikeDose):
-                    raise KeyError("All list items must be SpikedDose objects.")
-        self._dosing_strategy = dosing_strategy
+                if not isinstance(dose, Dose):
+                    raise TypeError("List must contain Dose or Spiked objects.")
+
