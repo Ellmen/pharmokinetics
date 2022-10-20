0f7463b3484862ff419d6c32539deca34c51b58f
Ramanspec <115243223+Ramanspec@users.noreply.github.com>
2022-10-20 15:39:20 +0100

    Added docstrings
    Please check this carefully, especially if the numerical data assigned by me (list, float, string etc.) is correct.

--- pkmodel/dose.py
+++ pkmodel/dose.py
@@ -3,6 +3,16 @@ from math import inf
 
 class Dose:
     def __init__(self, rate, start=0, end=inf):
+        """Describe continuously administered dose
+
+        Args:
+            rate (list): list of doses specifying intervals
+                between time and the dose administered.
+            start (float): timepoint at which drug administration
+                starts, default at the start of the model.
+            end (float): timepoint at which drug administration
+                ends, default is administration that never ends.
+        """
         self.start = start
         self.end = end
         self.rate = rate
@@ -10,6 +20,14 @@ class Dose:
 
 class SpikeDose(Dose):
     def __init__(self, volume, start):
+        """Describe instantenously administered drug dose(s)
+
+        Args:
+            volume (float): the total administered drug for a single
+                instantenous injection.
+            start (float): timepoint at which drug administration
+                starts.
+        """
         delta = 0.01
         end = start + delta
         rate = volume / delta
